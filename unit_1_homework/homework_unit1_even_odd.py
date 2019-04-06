33ed2198b2ae10fcff718f16ac7f5c65d5886f12
upstart <erinchurch6@gmail.com>
2019-04-05 22:26:31 -0400

    updates for additional assignments.

---
+++ unit_1_homework/homework_unit1_even_odd.py
@@ -0,0 +1,14 @@
+"""
+completed after week unit2, only completed for personal interest.
+
+Ask the user for a number.
+Depending on whether the number is even or odd, print out an appropriate message to the user.
+
+"""
+
+user_input = int(input("Please provide a number to verify if even or odd.\t"))
+
+if user_input % 2 == 0:
+    print("The value you provided is an even number.")
+else:
+    print("The value you provided is an odd number.")
